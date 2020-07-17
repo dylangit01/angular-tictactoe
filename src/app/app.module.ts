255d4e84d4a84ec4b388bd08f6862c0ff26a4a72
Dylan <dcren2010@gmail.com>
2020-07-17 12:57:51 -0400

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule,
